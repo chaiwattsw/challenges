837031fc475a14b28c5ff2e423d48a97727eeef9
Chaiwat Trisuwan <chaiwattsw@gmail.com>
2024-09-23 02:10:50 +0700

    feat: handling image configuration

--- challenge-react/webpack.config.js
+++ challenge-react/webpack.config.js
@@ -41,6 +41,13 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]'
+        }
+      },
     ],
   },
 
