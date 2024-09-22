19fc29f2d52756f96de9086f63d59a11299ec7e8
Chaiwat Trisuwan <chaiwattsw@gmail.com>
2024-09-23 01:37:24 +0700

    feature: migrate entry file to TypeScript

---
+++ challenge-react/src/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './App';
+
+interface AppState {
+  donate: number;
+  message: string;
+}
+
+type Action =
+  | { type: 'UPDATE_TOTAL_DONATE'; amount: number }
+  | { type: 'UPDATE_MESSAGE'; message: string };
+
+const reducer = (state: AppState | undefined, action: Action): AppState => {
+  const _state: AppState = state ?? {
+    donate: 0,
+    message: '',
+  };
+
+  switch (action.type) {
+    case 'UPDATE_TOTAL_DONATE':
+      return {
+        ..._state,
+        donate: _state.donate + action.amount,
+      };
+    case 'UPDATE_MESSAGE':
+      return {
+        ..._state,
+        message: action.message,
+      };
+    default:
+      return _state;
+  }
+};
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
