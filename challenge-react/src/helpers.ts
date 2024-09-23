b87b2fea009ee4e4245e133af63c9850fddfec85
Chaiwat Trisuwan <chaiwattsw@gmail.com>
2024-09-23 09:47:22 +0700

    feat: add formatCurrency helper function

--- challenge-react/src/helpers.ts
+++ challenge-react/src/helpers.ts
@@ -3,3 +3,12 @@ export const summaryDonations = (donations: number[]): number =>
     (accumulator: number, value: number) => accumulator + value,
     0
   );
+
+export const formatCurrency = (amount: number, currency: string = 'THB') => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency,
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(amount);
+};
